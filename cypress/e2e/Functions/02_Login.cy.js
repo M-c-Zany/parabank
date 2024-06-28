d402eeb8a58a8fd6473372ea60d01664ee5d9459
vadde-thirumaleshh <vadde.thirumalesh2001@gmail.com>
2024-06-28 23:16:09 +0530

    thiru01

--- cypress/e2e/Functions/02_Login.cy.js
+++ cypress/e2e/Functions/02_Login.cy.js
@@ -1,5 +1,8 @@
 export class Login{
     loginPage(){
         
+        cy.get(".input").eq(0).type("rzYEOZ6P");
+        cy.get(".input").eq(1).type("OqAd2BKE");
+        cy.get(".button").eq(1).click();
     }
 }
